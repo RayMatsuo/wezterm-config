463842a91007c9b90ffca4b2a8057dd74f009414
DESKTOP-9DK0RH9\matsu <raymatsuo7@gmail.com>
2024-04-04 15:52:32 +0900

    UPDATE:added easy theme switching

---
+++ presets.lua
@@ -0,0 +1,20 @@
+
+Void = {
+  {
+    -- source = { File = "C:/Users/matsu/.config/wezterm/img/signalis_logo.png", },
+    source = { File = "C:/Users/matsu/.config/wezterm/img/votv.png", },
+    width = "Cover",
+    horizontal_align = "Center",
+    hsb = { brightness = 0.2, },
+  },
+}
+Derangement = {
+  { source = { Color = "black" }, width = "100%", height = "100%" },
+  {
+    source = { File = { path = "C:/Users/matsu/.config/wezterm/img/derangement_1.apng", speed = 1 } },
+    width = "Cover",
+    horizontal_align = "Center",
+    hsb = { brightness = 0.03 },
+    -- attachment={Parallax=0.2}
+  },
+}
--- wezterm.lua
+++ wezterm.lua
@@ -10,41 +10,16 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
+require "presets"
 
--- For example, changing the color scheme:
+-- NOTE: Switch background presets here
+config.background =Derangement
+
+
+
+
 config.color_scheme = "AdventureTime"
-
 config.enable_kitty_graphics = true
-
 config.font_size = 11
--- config.window_background_image = "C:/Users/matsu/.config/wezterm/img/signalis_logo.png"
-
-local bg_image = {
-  {
-    -- source = { File = "C:/Users/matsu/.config/wezterm/img/signalis_logo.png", },
-    source = { File = "C:/Users/matsu/.config/wezterm/img/votv.png", },
-    width = "Cover",
-    horizontal_align = "Center",
-    hsb = { brightness = 0.3, },
-  },
-}
-local bg_animated = {
-  { source = { Color = "black" }, width = "100%", height = "100%" },
-  {
-    source = { File = { path = "C:/Users/matsu/.config/wezterm/img/derangement_1.apng", speed = 1 } },
-    width = "Cover",
-    horizontal_align = "Center",
-    hsb = { brightness = 0.03 },
-    -- attachment={Parallax=0.2}
-  },
-}
-
-config.background = bg_animated
-
-config.window_background_image_hsb = {}
 config.default_prog = { "C:/Program Files/PowerShell/7/pwsh.exe", "-NoLogo" }
-
-require("splits")
-
 return config
