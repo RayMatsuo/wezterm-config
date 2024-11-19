f39ba661d7752373c131a5625428ebfa552f6268
RayMatsuo <raymatsuo7@gmail.com>
2024-11-19 18:29:07 +0900

    UPDATE:ruina wallpapers

--- presets.lua
+++ presets.lua
@@ -1,63 +1,88 @@
-local home=os.getenv("HOME")
-Void = {
+local home = os.getenv("HOME")
+Natural = {
   {
-    -- source = { File = home.."/.config/wezterm/img/signalis_logo.png", },
-    source = { File = home.."/.config/wezterm/img/votv.png", },
+    source = { File = home .. "/.config/wezterm/img/natural_science.jpg" },
     width = "Cover",
     horizontal_align = "Center",
-    hsb = { brightness = 0.2, },
+    hsb = { brightness = 0.1 },
+  },
+}
+Library = {
+  {
+    source = { File = home .. "/.config/wezterm/img/library.jpg" },
+    width = "Cover",
+    horizontal_align = "Center",
+    hsb = { brightness = 0.1 },
+  },
+}
+Void = {
+  {
+    source = { File = home .. "/.config/wezterm/img/votv.png" },
+    width = "Cover",
+    horizontal_align = "Center",
+    hsb = { brightness = 0.2 },
   },
 }
 
 Thunderstorm = {
   {
-    source = { File = home.."/.config/wezterm/img/thunderstorm2.png", },
+    source = { File = home .. "/.config/wezterm/img/thunderstorm2.png" },
     width = "Cover",
     -- horizontal_align = "Center",
-    vertical_align="Top",
-    hsb = { brightness = 0.1, },
+    vertical_align = "Top",
+    hsb = { brightness = 0.1 },
+  },
+}
+
+Thunderstorm2 = {
+  {
+    source = { File = home .. "/.config/wezterm/img/Thunderstorm.jpg" },
+    width = "Cover",
+    -- horizontal_align = "Center",
+    vertical_align = "Top",
+    hsb = { brightness = 0.1 },
   },
 }
 Ocean = {
   {
     -- source = { File = home.."/.config/wezterm/img/signalis_logo.png", },
-    source = { File = home.."/.config/wezterm/img/ocean.jpg", },
+    source = { File = home .. "/.config/wezterm/img/ocean.jpg" },
     height = "Cover",
     vertical_align = "Bottom",
-    hsb = { brightness = 0.2, },
+    hsb = { brightness = 0.2 },
   },
 }
 Lantern = {
   {
     -- source = { File = home.."/.config/wezterm/img/signalis_logo.png", },
-    source = { File = home.."/.config/wezterm/img/lantern.jpg", },
+    source = { File = home .. "/.config/wezterm/img/lantern.jpg" },
     height = "Cover",
     vertical_align = "Bottom",
-    hsb = { brightness = 0.2, },
+    hsb = { brightness = 0.2 },
   },
 }
 Sesbian = {
   {
     -- source = { File = home.."/.config/wezterm/img/signalis_logo.png", },
-    source = { File = home.."/.config/wezterm/img/signalis_logo.png", },
+    source = { File = home .. "/.config/wezterm/img/signalis_logo.png" },
     width = "Cover",
     horizontal_align = "Center",
-    hsb = { brightness = 0.5, },
+    hsb = { brightness = 0.5 },
   },
 }
 
 Kco = {
   {
-    source = { File = home.."/.config/wezterm/img/kco.png", },
+    source = { File = home .. "/.config/wezterm/img/kco.png" },
     height = "Cover",
     vertical_align = "Bottom",
-    hsb = { brightness = 0.1, },
+    hsb = { brightness = 0.1 },
   },
 }
 Derangement = {
   { source = { Color = "black" }, width = "100%", height = "100%" },
   {
-    source = { File = { path = home.."/.config/wezterm/img/derangement_1.apng", speed = 1 } },
+    source = { File = { path = home .. "/.config/wezterm/img/derangement_1.apng", speed = 1 } },
     width = "Cover",
     horizontal_align = "Center",
     hsb = { brightness = 0.03 },
--- wezterm.lua
+++ wezterm.lua
@@ -3,8 +3,6 @@ local wezterm = require("wezterm")
 -- This table will hold the configuration.
 config = {}
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
 if wezterm.config_builder then
   config = wezterm.config_builder()
 end
@@ -13,8 +11,7 @@ config.enable_kitty_graphics = true
 config.font_size = 11
 config.default_prog = { "C:/Program Files/PowerShell/7/pwsh.exe", "-NoLogo" }
 require("presets")
--- require("splits")
 
--- NOTE: Switch background presets here
-config.background = Thunderstorm
+config.background = Natural
+
 return config
